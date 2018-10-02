ff88c25b1c37f5dd6d502143f3bed17be7bf01ed
Alexander Osborn <misterzander@gmail.com>
2018-10-02 15:50:47 -0500

    Add links

---
+++ assets/scripts/script.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyBQG61VsAlqf40ck2e1pXKTsOt-fQGh1-o",
+    authDomain: "rps-data-f162d.firebaseapp.com",
+    databaseURL: "https://rps-data-f162d.firebaseio.com",
+    projectId: "rps-data-f162d",
+    storageBucket: "rps-data-f162d.appspot.com",
+    messagingSenderId: "1044015908466"
+  };
+  firebase.initializeApp(config);
