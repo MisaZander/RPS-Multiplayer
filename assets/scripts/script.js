28a96602c947f737d165bdb6c5157e345074d9e9
Alexander Osborn <misterzander@gmail.com>
2018-10-03 12:59:51 -0500

    Add a player to the cloud

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -7,3 +7,34 @@ var config = {
     messagingSenderId: "1044015908466"
   };
   firebase.initializeApp(config);
+  var db = firebase.database();
+  var ref;
+
+  var player = {
+      playerID: 0,
+      opponentID: 0,
+      totalWins: 0,
+      totalLosses: 0,
+      wins: 0,
+      losses: 0,
+      userName: "",
+      queue: {
+          choice: "",
+          taunt: ""
+      }
+  };
+
+  //enqueue
+  $("#enqueue").on("click", function(event) {
+    event.preventDefault();  
+    let userName = $("#userName").val();
+    //userName = "Cockus Blackamus";
+    player.userName = userName;
+    db.ref().push(player);
+  });
+
+  db.ref().on("value", function(snapshot) {
+    ref = snapshot.val();
+    console.log(ref);
+  }); //db ref on
+
